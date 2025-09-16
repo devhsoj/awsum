e3d6c8dfeb39a6dd5078507a5ee74bf8dc1c7200
Joshua Benfield <josh@benfield.org>
2025-09-16 19:21:16 -0400

    reworking bad fs implementation, file storage improved

--- commands/file.go
+++ commands/file.go
@@ -1,59 +1,22 @@
 package commands
 
 import (
+    "bytes"
     "context"
-    "encoding/csv"
     "fmt"
-    "io"
     "os"
     "path"
-    "time"
 
     "github.com/aws/aws-sdk-go-v2/aws"
     "github.com/aws/aws-sdk-go-v2/service/s3"
     "github.com/aws/aws-sdk-go-v2/service/s3/types"
+    "github.com/devhsoj/awsum/internal/files"
     "github.com/devhsoj/awsum/internal/mem"
     "github.com/devhsoj/awsum/service"
-
-    "github.com/olekukonko/tablewriter"
 )
 
-func StoreFile(ctx context.Context, awsConfig aws.Config, filename string, prefix string) error {
-    svc := s3.NewFromConfig(awsConfig)
-
-    f, err := os.OpenFile(filename, os.O_RDONLY, 0400)
-
-    if err != nil {
-        return fmt.Errorf("failed to open file '%s' to store in bucket: %w", filename, err)
-    }
-
-    defer func() {
-        if err := f.Close(); err != nil {
-            fmt.Printf("failed to properly close file to be stored in s3: %s\n", err)
-        }
-    }()
-
-    bucket, err := service.GetBucketByName(ctx, awsConfig, service.S3FileBucketName)
-
-    if err != nil {
-        return err
-    }
-
-    _, err = svc.PutObject(ctx, &s3.PutObjectInput{
-        Bucket: bucket.S3.Name,
-        Key:    mem.Pointer(path.Join(prefix, path.Base(filename))),
-        ACL:    types.ObjectCannedACLPrivate,
-        Body:   f,
-    })
-
-    if err != nil {
-        return fmt.Errorf("failed to put file object: %w", err)
-    }
-
-    return nil
-}
-
-func RetrieveFile(ctx context.Context, awsConfig aws.Config, filename string, prefix string) error {
+func StoreFileItems(ctx context.Context, awsConfig aws.Config, prefix string, items ...string) error {
+    workingPath := path.Dir(prefix)
     svc := s3.NewFromConfig(awsConfig)
 
     bucket, err := service.GetBucketByName(ctx, awsConfig, service.S3FileBucketName)
@@ -62,92 +25,55 @@ func RetrieveFile(ctx context.Context, awsConfig aws.Config, filename string, pr
         return err
     }
 
-    output, err := svc.GetObject(ctx, &s3.GetObjectInput{
-        Bucket: bucket.S3.Name,
-        Key:    mem.Pointer(path.Join(prefix, path.Base(filename))),
-    })
+    for _, item := range items {
+        itemPath := path.Join(workingPath, item)
+        itemInfo, err := os.Stat(itemPath)
 
-    if err != nil {
-        return fmt.Errorf("failed to get file s3 object: %w", err)
-    }
-
-    if _, err := io.Copy(os.Stdout, output.Body); err != nil {
-        return fmt.Errorf("failed to write file s3 object content to stdout: %w", err)
-    }
-
-    return nil
-}
-
-func DeleteFile(ctx context.Context, awsConfig aws.Config, filename string, prefix string) error {
-    svc := s3.NewFromConfig(awsConfig)
-
-    bucket, err := service.GetBucketByName(ctx, awsConfig, service.S3FileBucketName)
-
-    if err != nil {
-        return err
-    }
-
-    _, err = svc.DeleteObject(ctx, &s3.DeleteObjectInput{
-        Bucket: bucket.S3.Name,
-        Key:    mem.Pointer(path.Join(prefix, path.Base(filename))),
-    })
-
-    if err != nil {
-        return fmt.Errorf("failed to delete file s3 object: %w", err)
-    }
-
-    return nil
-}
-
-func ListFiles(ctx context.Context, config aws.Config, format string) error {
-    files, err := service.GetAllBucketObjects(ctx, config, service.S3FileBucketName)
-
-    if err != nil {
-        return fmt.Errorf("failed to get all files: %w", err)
-    }
-
-    if format == "csv" {
-        w := csv.NewWriter(os.Stdout)
-
-        if err = w.Write([]string{
-            "Filename",
-            "FileSizeInBytes",
-            "DateLastModifiedInSeconds",
-        }); err != nil {
-            return fmt.Errorf("failed to write file header csv record: %w", err)
+        if err != nil {
+            return fmt.Errorf("failed to stat item before storing in s3: %w", err)
         }
 
-        for _, file := range files {
-            if err = w.Write([]string{
-                mem.Unwrap(file.Key),
-                fmt.Sprintf("%d", mem.Unwrap(file.Size)),
-                fmt.Sprintf("%d", mem.Unwrap(file.LastModified).Unix()),
-            }); err != nil {
-                return fmt.Errorf("failed to write file csv record: %w", err)
+        if itemInfo.IsDir() {
+            entries, err := os.ReadDir(itemInfo.Name())
+
+            if err != nil {
+                return fmt.Errorf("failed to read dir entries before storing in s3: %w", err)
             }
+
+            var entryItems []string
+
+            for _, entry := range entries {
+                entryItems = append(entryItems, path.Join(itemInfo.Name(), entry.Name()))
+            }
+
+            if err = StoreFileItems(
+                ctx,
+                awsConfig,
+                path.Join(prefix, itemInfo.Name()),
+                entryItems...,
+            ); err != nil {
+                return err
+            }
+
+            continue
         }
 
-        w.Flush()
-    } else if format == "pretty" {
-        table := tablewriter.NewWriter(os.Stdout)
+        buf, err := files.ReadFileFull(itemPath)
 
-        table.Header([]string{
-            "Name",
-            "Size",
-            "Date Last Modified",
+        if err != nil {
+            return err
+        }
+
+        _, err = svc.PutObject(ctx, &s3.PutObjectInput{
+            Bucket: bucket.S3.Name,
+            Key:    mem.Pointer(path.Join(prefix, itemInfo.Name())),
+            ACL:    types.ObjectCannedACLPrivate,
+            Body:   bytes.NewReader(buf),
         })
 
-        for _, file := range files {
-            if err = table.Append([]string{
-                mem.Unwrap(file.Key),
-                fmt.Sprintf("%.2f MiB", (float64(mem.Unwrap(file.Size)))/1_024/1_024),
-                mem.Unwrap(file.LastModified).In(time.Local).Format(time.RFC850),
-            }); err != nil {
-                return fmt.Errorf("failed to build file list table: %w", err)
-            }
+        if err != nil {
+            return fmt.Errorf("failed to store s3 file object: %w", err)
         }
-
-        return table.Render()
     }
 
     return nil
--- main.go
+++ main.go
@@ -1,194 +1,180 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
-	"strings"
+    "context"
+    "fmt"
+    "os"
+    "strings"
 
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/devhsoj/awsum/commands"
-	"github.com/devhsoj/awsum/service"
-	"github.com/urfave/cli/v3"
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/devhsoj/awsum/commands"
+    "github.com/devhsoj/awsum/service"
+    "github.com/urfave/cli/v3"
 )
 
 func main() {
-	ctx := context.Background()
+    ctx := context.Background()
 
-	awsConfig, err := config.LoadDefaultConfig(ctx)
+    awsConfig, err := config.LoadDefaultConfig(ctx)
 
-	if err != nil {
-		fmt.Printf("failed to load aws config: %s\n", err)
-		os.Exit(1)
-	}
+    if err != nil {
+        fmt.Printf("failed to load aws config: %s\n", err)
+        os.Exit(1)
+    }
 
-	cmd := &cli.Command{
-		Name:        "awsum",
-		Description: "awsum is a fun CLI tool for managing AWS infrastructure at a high level",
-		HideHelp:    true,
-		Action: func(ctx context.Context, command *cli.Command) error {
-			return commands.Intro(awsConfig)
-		},
-		Commands: []*cli.Command{
-			{
-				Name:        "help",
-				Description: "show helpful information about awsum and how it works",
-				Action: func(ctx context.Context, command *cli.Command) error {
-					return commands.Help()
-				},
-			},
-			{
-				Name: "instance",
-				Commands: []*cli.Command{
-					{
-						Name:        "list",
-						Description: "list EC2 instances",
-						Flags: []cli.Flag{
-							&cli.StringFlag{
-								Name:     "format",
-								Usage:    "pretty|csv",
-								Value:    "pretty",
-								OnlyOnce: true,
-								Validator: func(s string) error {
-									if s != "pretty" && s != "csv" {
-										return fmt.Errorf("invalid format, must be pretty or csv")
-									}
+    cmd := &cli.Command{
+        Name:        "awsum",
+        Description: "awsum is a fun CLI tool for managing AWS infrastructure at a high level",
+        HideHelp:    true,
+        Action: func(ctx context.Context, command *cli.Command) error {
+            return commands.Intro(awsConfig)
+        },
+        Commands: []*cli.Command{
+            {
+                Name:        "help",
+                Description: "show helpful information about awsum and how it works",
+                Action: func(ctx context.Context, command *cli.Command) error {
+                    return commands.Help()
+                },
+            },
+            {
+                Name: "instance",
+                Commands: []*cli.Command{
+                    {
+                        Name:        "list",
+                        Description: "list EC2 instances",
+                        Flags: []cli.Flag{
+                            &cli.StringFlag{
+                                Name:     "format",
+                                Usage:    "pretty|csv",
+                                Value:    "pretty",
+                                OnlyOnce: true,
+                                Validator: func(s string) error {
+                                    if s != "pretty" && s != "csv" {
+                                        return fmt.Errorf("invalid format, must be pretty or csv")
+                                    }
 
-									return nil
-								},
-								ValidateDefaults: true,
-							},
-						},
-						Action: func(ctx context.Context, command *cli.Command) error {
-							return commands.List(ctx, awsConfig, command.String("format"))
-						},
-					},
-					{
-						Name:        "shell",
-						Description: "run a command or start a shell (via SSH) on EC2 instances matched by the given filters",
-						Flags: []cli.Flag{
-							&cli.StringFlag{
-								Name:     "user",
-								Aliases:  []string{"as"},
-								Usage:    "which ssh user to connect as",
-								Value:    "ec2-user",
-								OnlyOnce: true,
-							},
-							&cli.StringFlag{
-								Name:     "name",
-								Usage:    "a fuzzy filter that matches against EC2 instance names (from tags)",
-								OnlyOnce: true,
-							},
-						},
-						Action: func(ctx context.Context, command *cli.Command) error {
-							return commands.StartShell(commands.StartShellOptions{
-								Ctx:       ctx,
-								AWSConfig: awsConfig,
-								InstanceFilters: service.InstanceFilters{
-									Name: command.String("name"),
-								},
-								User:    command.String("user"),
-								Command: strings.Join(command.Args().Slice(), " "),
-							})
-						},
-					},
-				},
-			},
-			{
-				Name: "file",
-				Commands: []*cli.Command{
-					{
-						Name: "store",
-						Flags: []cli.Flag{
-							&cli.StringFlag{
-								Name: "prefix",
-							},
-						},
-						Action: func(ctx context.Context, command *cli.Command) error {
-							prefix := command.String("prefix")
+                                    return nil
+                                },
+                                ValidateDefaults: true,
+                            },
+                        },
+                        Action: func(ctx context.Context, command *cli.Command) error {
+                            return commands.List(ctx, awsConfig, command.String("format"))
+                        },
+                    },
+                    {
+                        Name:        "shell",
+                        Description: "run a command or start a shell (via SSH) on EC2 instances matched by the given filters",
+                        Flags: []cli.Flag{
+                            &cli.StringFlag{
+                                Name:     "user",
+                                Aliases:  []string{"as"},
+                                Usage:    "which ssh user to connect as",
+                                Value:    "ec2-user",
+                                OnlyOnce: true,
+                            },
+                            &cli.StringFlag{
+                                Name:     "name",
+                                Usage:    "a fuzzy filter that matches against EC2 instance names (from tags)",
+                                OnlyOnce: true,
+                            },
+                        },
+                        Action: func(ctx context.Context, command *cli.Command) error {
+                            return commands.StartShell(commands.StartShellOptions{
+                                Ctx:       ctx,
+                                AWSConfig: awsConfig,
+                                InstanceFilters: service.InstanceFilters{
+                                    Name: command.String("name"),
+                                },
+                                User:    command.String("user"),
+                                Command: strings.Join(command.Args().Slice(), " "),
+                            })
+                        },
+                    },
+                },
+            },
+            {
+                Name: "file",
+                Commands: []*cli.Command{
+                    {
+                        Name: "store",
+                        Flags: []cli.Flag{
+                            &cli.StringFlag{
+                                Name:      "prefix",
+                                Usage:     "",
+                                Required:  false,
+                                Aliases:   []string{"p"},
+                                Validator: nil,
+                            },
+                        },
+                        Action: func(ctx context.Context, command *cli.Command) error {
+                            return commands.StoreFileItems(
+                                ctx,
+                                awsConfig,
+                                command.String("prefix"),
+                                command.Args().Slice()...,
+                            )
+                        },
+                    },
+                    {
+                        Name:    "retrieve",
+                        Aliases: []string{"get", "fetch"},
+                        Arguments: []cli.Argument{
+                            &cli.StringArg{
+                                Name:      "key",
+                                UsageText: "The key to match (which is the filename or directory path when files are stored from awsum) of the file s3 object(s) to be retrieved.",
+                            },
+                        },
+                        Action: func(ctx context.Context, command *cli.Command) error {
+                            return nil
+                        },
+                    },
+                    {
+                        Name: "delete",
+                        Arguments: []cli.Argument{
+                            &cli.StringArg{
+                                Name:      "filename",
+                                UsageText: "The filename of the file s3 object.",
+                            },
+                            &cli.StringArg{
+                                Name:      "prefix",
+                                UsageText: "The S3 prefix to prepend to the filename for the resulting S3 object. Commonly used to represent directories.",
+                            },
+                        },
+                        Action: func(ctx context.Context, command *cli.Command) error {
+                            return nil
+                        },
+                    },
+                    {
+                        Name: "list",
+                        Flags: []cli.Flag{
+                            &cli.StringFlag{
+                                Name:     "format",
+                                Usage:    "pretty|csv",
+                                Value:    "pretty",
+                                OnlyOnce: true,
+                                Validator: func(s string) error {
+                                    if s != "pretty" && s != "csv" {
+                                        return fmt.Errorf("invalid format, must be pretty or csv")
+                                    }
 
-							for _, filename := range command.Args().Slice() {
-								if err = commands.StoreFile(
-									ctx,
-									awsConfig,
-									filename,
-									prefix,
-								); err != nil {
-									return err
-								}
+                                    return nil
+                                },
+                                ValidateDefaults: true,
+                            },
+                        },
+                        Action: func(ctx context.Context, command *cli.Command) error {
+                            return nil
+                        },
+                    },
+                },
+            },
+        },
+    }
 
-								fmt.Printf("stored %s\n", filename)
-							}
-
-							return nil
-						},
-					},
-					{
-						Name: "retrieve",
-						Arguments: []cli.Argument{
-							&cli.StringArg{
-								Name:      "filename",
-								UsageText: "The filename of the file to be retrieved. Absolute and relative paths are allowed.",
-							},
-							&cli.StringArg{
-								Name:      "prefix",
-								UsageText: "The S3 prefix to prepend to the filename for the resulting S3 object. Commonly used to represent directories.",
-							},
-						},
-						Action: func(ctx context.Context, command *cli.Command) error {
-							return commands.RetrieveFile(
-								ctx,
-								awsConfig,
-								command.StringArg("filename"),
-								command.StringArg("prefix"),
-							)
-						},
-					},
-					{
-						Name: "delete",
-						Arguments: []cli.Argument{
-							&cli.StringArg{
-								Name:      "filename",
-								UsageText: "The filename of the file s3 object.",
-							},
-							&cli.StringArg{
-								Name:      "prefix",
-								UsageText: "The S3 prefix to prepend to the filename for the resulting S3 object. Commonly used to represent directories.",
-							},
-						},
-						Action: func(ctx context.Context, command *cli.Command) error {
-							return commands.DeleteFile(ctx, awsConfig, command.StringArg("filename"), command.StringArg("prefix"))
-						},
-					},
-					{
-						Name: "list",
-						Flags: []cli.Flag{
-							&cli.StringFlag{
-								Name:     "format",
-								Usage:    "pretty|csv",
-								Value:    "pretty",
-								OnlyOnce: true,
-								Validator: func(s string) error {
-									if s != "pretty" && s != "csv" {
-										return fmt.Errorf("invalid format, must be pretty or csv")
-									}
-
-									return nil
-								},
-								ValidateDefaults: true,
-							},
-						},
-						Action: func(ctx context.Context, command *cli.Command) error {
-							return commands.ListFiles(ctx, awsConfig, command.String("format"))
-						},
-					},
-				},
-			},
-		},
-	}
-
-	if err = cmd.Run(ctx, os.Args); err != nil {
-		fmt.Printf("failed to run command: %s\n", err)
-		os.Exit(1)
-	}
+    if err = cmd.Run(ctx, os.Args); err != nil {
+        fmt.Printf("failed to run command: %s\n", err)
+        os.Exit(1)
+    }
 }
